40e13a023e0dbf5cda59315d305de6899c435ee1
Adam Zieman <adamzieman@yahoo.com>
2024-12-12 13:35:20 -0600

    Improved comments, variable names, and error messages

--- WIN11NonCompatibleAppsHardware.bat
+++ WIN11NonCompatibleAppsHardware.bat
@@ -1,216 +1,176 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM Initializes variables to define the path to Axonius reports and checks for the existence of relevant directories and files.
+REM Prompt the user to input the report date, constructs the path for the specific report, and verifies its existence.
+REM Constructs the path to the "WIN11 Incompatible Apps and Hardware" CSV file and checks if the file exists.
+REM If any path or file is missing, the script prints an error message and either jumps to an error handling label or prompts the user again for input.
 
-REM Instatiate a working absolute Path
-set workingAbsolutePath=''
+set "axoniusReportsParentDirectory=C:\Users\d18284\LocalDocuments\Projects\WIN11\AxoniusReports\"
+set workingPath=%axoniusReportsParentDirectory%
 
-
-REM Instantiate the path to the parent directory for the Axonius reports
-set "axoniusReportsPath=C:\Users\d18284\LocalDocuments\Projects\WIN11\AxoniusReports\"
-set workingAbsolutePath=!axoniusReportsPath!
-if not exist "%workingAbsolutePath%" (
-    echo Error: Path does not exist.
-    echo %axoniusReportsPath%
-	
-    goto :EndError
+if not exist "%workingPath%" (
+    echo Error: Parent directory for Axonius reports does not exist.
+    echo %workingPath%
+    goto :ExitOnError
 )
 
+:GetReportDate
 
-REM Function to get the date for the Axonius report
-:GetAxoniusReportDate
+set /p reportDate="Enter the date of the Axonius report in YYYY_MM_DD format: "
+set "axoniusReportTargetedDirectory=Axonius Report !reportDate!\"
+set workingPath=%workingPath%!axoniusReportTargetedDirectory!
 
-
-REM Get the date of the Axonius report from the user
-set /p axoniusReportDate="Enter the date of the Axonius report in YYYY_MM_DD format: "
-
-
-REM Instantiate the Axonius report directory
-set "targetedReportDirectory=Axonius Report !axoniusReportDate!\"
-set set workingAbsolutePath=!axoniusReportsPath!!targetedReportDirectory!
-
-
-REM Check if the required files exist
-if not exist !workingAbsolutePath! (
-	echo Error: Directory does not exist.
-	echo %targetedReportDirectory%
+if not exist !workingPath! (
+	echo Error: Directory for specified Axonius report does not exist.
+	echo !workingPath!
 	echo.
-	echo Absolute path: !workingAbsolutePath!
-	echo.
-	goto :GetAxoniusReportDate
+	goto :GetReportDate
 )
 
+set "incompatibleAppsHardwareCSV=Windows 11 Not Compatible Apps and Hardware*"
+set workingPath=!workingPath!%incompatibleAppsHardwareCSV%
 
-REM Instantiate the csv Axonius report
-set "targetedCSV=Windows 11 Not Compatible Apps and Hardware*"
-set workingAbsolutePath=!axoniusReportsPath!!targetedReportDirectory!!targetedCSV!
-if not exist "%workingAbsolutePath%" (
-    echo Error: File does not exist.
-	echo %targetedCSV%
-	echo.
-	echo Absolute path: !workingAbsolutePath!
-    echo .
-	
-    goto :EndError
+if not exist "!workingPath!" (
+    echo Error: WIN11 Incompatible Apps and Hardware CSV file does not exist.
+	echo !workingPath!
+    goto :ExitOnError
 )
 
+set "targetedCsvField=Sccm: Collections"=
 
-REM Instantiate the column to search in the csv file
-set "targetedColumn=Sccm: Collections"=
-
-
-REM Prints which Axonius directory and file is being searched
-echo %targetedReportDirectory%%targetedCSV%
+echo !workingPath!
 
 
-REM Creates temp files
-set NonCompatibleFile=%axoniusReportsPath%temp_NonCompatible.txt
-> %NonCompatibleFile% echo.
-set UniqueNonCompatibleComputersFile=%axoniusReportsPath%temp_UniqueNonCompatibleComputers.txt
-> %UniqueNonCompatibleComputersFile% echo.
-set UniqueNonCompatibleAppsFile=%axoniusReportsPath%temp_UniqueNonCompatibleApps.txt
-> %UniqueNonCompatibleAppsFile% echo.
 
+REM Initializes temporary files to store SCCM collections, incompatible hardware, and incompatible software data.
+REM Extracts data from a specified CSV file using PowerShell, removes duplicate entries, and classifies the entries into either incompatible hardware or software based on the content.
+REM The script checks for entries related to "Dell" or "HP" and appends them to the respective temporary file.
+REM After processing, the temporary SCCM collections file is deleted.
 
-REM Creates output files
-set SortedUniqueNonCompatibleComputersFile=%axoniusReportsPath%NonCompatibleComputers.txt
-> %SortedUniqueNonCompatibleComputersFile% echo.
-set SortedUniqueNonCompatibleAppsFile=%axoniusReportsPath%NonCompatibleApps.txt
-> %SortedUniqueNonCompatibleAppsFile% echo.
+set tempSccmCollectionsFile=%axoniusReportsParentDirectory%temp_sccmCollections.txt
+> %tempSccmCollectionsFile% echo.
+set tempIncompatibleHardwareFile=%axoniusReportsParentDirectory%temp_incompatibleHardware.txt
+> %tempIncompatibleHardwareFile% echo.
+set tempIncompatibleSoftwareFile=%axoniusReportsParentDirectory%temp_incompatibleSoftware.txt
+> %tempIncompatibleSoftwareFile% echo.
 
-
-REM Prints that the script will begin extracting data
 echo ... Extracting data
 
+powershell -Command "Import-Csv -Path '!workingPath!' | Select-Object -Unique -ExpandProperty '%targetedCsvField%' | Out-File -FilePath '%tempSccmCollectionsFile%' -Encoding utf8"
 
-REM PowerShell command to extract values from the targeted column
-powershell -Command "Import-Csv -Path '%workingAbsolutePath%' | Select-Object -Unique -ExpandProperty '%targetedColumn%' | Out-File -FilePath '%NonCompatibleFile%' -Encoding utf8"
-
-
-REM Prints that the script will begin removing duplicate entries
 echo ... Removing duplicate entries
 
-
-REM Organizes the entries into two files based on content, without duplicate entries
-for /f "usebackq delims=" %%A in (%NonCompatibleFile%) do (
+for /f "usebackq delims=" %%A in (%tempSccmCollectionsFile%) do (
     set line=%%A
 
-    :: Trim spaces
     for /f "tokens=* delims=" %%B in ("!line!") do set "line=%%B"
-
-    :: Check if the line starts with 'Dell' or 'HP'
-    echo !line! | findstr /b /i "Dell HP" >nul
-    if !errorlevel! equ 0 (
-        REM Check if the line is unique in computersFile
-        findstr /x /c:"!line!" %UniqueNonCompatibleComputersFile% >nul
-        if !errorlevel! neq 0 (
-            >> %UniqueNonCompatibleComputersFile% echo(!line!
-        )
-    ) else (
-        REM Check if the line is unique in appsFile
-        findstr /x /c:"!line!" %UniqueNonCompatibleAppsFile% >nul
-        if !errorlevel! neq 0 (
-            >> %UniqueNonCompatibleAppsFile% echo(!line!
-        )
+		echo !line! | findstr /b /i "Dell HP" >nul
+		
+		if !errorlevel! equ 0 (
+			findstr /x /c:"!line!" %tempIncompatibleHardwareFile% >nul
+			
+			if !errorlevel! neq 0 (
+				>> %tempIncompatibleHardwareFile% echo(!line!
+			)
+		) else (
+			findstr /x /c:"!line!" %tempIncompatibleSoftwareFile% >nul
+			
+			if !errorlevel! neq 0 (
+				>> %tempIncompatibleSoftwareFile% echo(!line!
+			)
     )
 )
 
+del %tempSccmCollectionsFile%
 
-REM Prints that the script will begin sorting the entries alphanumerically
+
+
+REM Sorts the entries in the temporary incompatible hardware and software files alphanumerically and saves the sorted results to a output files.
+REM Deletes the temporary files and outputs the paths to the sorted hardware and software files, indicating the process is complete.
+
 echo ... Sorting alphanumerically
 
+set incompatibleHardwareFile=%axoniusReportsParentDirectory%incompatibleHardware.txt
+> %incompatibleHardwareFile% echo.
+set incompatibleSoftwareFile=%axoniusReportsParentDirectory%incompatibleSoftware.txt
+> %incompatibleSoftwareFile% echo.
 
-REM Sorts the contents of the unique, noncompatible files into their respective sorted, unique, noncompatible files.
-sort %UniqueNonCompatibleComputersFile% /o %SortedUniqueNonCompatibleComputersFile%
-sort %UniqueNonCompatibleAppsFile% /o %SortedUniqueNonCompatibleAppsFile%
+sort %tempIncompatibleHardwareFile% /o %incompatibleHardwareFile%
+sort %tempIncompatibleSoftwareFile% /o %IncompatibleSoftwareFile%
 
+del %tempIncompatibleSoftwareFile%
+del %tempIncompatibleHardwareFile%
 
-REM Deletes temp files and open the sorted, unique, noncompatible files
-del %NonCompatibleFile%
-del %UniqueNonCompatibleComputersFile%
-del %UniqueNonCompatibleAppsFile%
-
-
-REM Prints that the script is completed
-echo ... Batch completed
-
-
-REM Prints the path to the output files
-echo %axoniusReportsPath%%SortedUniqueNonCompatibleComputersFile%
-echo %axoniusReportsPath%%SortedUniqueNonCompatibleAppsFile%
-echo.
+echo ... Completed
+echo %axoniusReportsParentDirectory%%incompatibleHardwareFile%
+echo %axoniusReportsParentDirectory%%incompatibleSoftwareFile%
 
 
-REM Function to ask whether the user would like to compare current report to a previous report
-:CompareFile
 
+REM Prompts the user to decide whether they want to compare a file.
+REM If the user chooses to compare, it checks for the existence of the required previous reports based on the provided date.
+REM If the files exist, it compares the current and previous reports, appending the differences to a file and opening it in Notepad.
+REM If the user chooses not to compare or provides invalid input, the script exits or prompts again.
 
-REM Ask the user if they would like to view changes from a previous report
 set /p isCompareFile="Would you like to compare a file? (y/n)? "
 
+:CompareFile
+
+echo.
+
+set "previousAxoniusReportOutputs=%axoniusReportsParentDirectory%PreviousOutputs\"
+
 if /i "!isCompareFile!"=="y" (
-    REM Instantiate the path to the parent directory for the previous outputs
-    set "previousOutputsPath=%axoniusReportsPath%\PreviousOutputs\"
-    
-    if not exist "!previousOutputsPath!" (
-        echo Error: Path does not exist.
-        echo "!previousOutputsPath!"
-        goto :EndError
+    if not exist "%previousAxoniusReportOutputs%" (
+		echo Error: Directory for previous Axonius report outputs does not exist.
+		echo "%axoniusReportsParentDirectory%"
+        echo "%previousAxoniusReportOutputs%"
+        goto :ExitOnError
     )
     
-    REM Instantiate the date of the previous report you would like to compare
     set /p compareDate="Enter the date of the file you would like to compare in YYYY_MM_DD format: "
-    
-    REM Check if the required files exist
-    if not exist "!previousOutputsPath!\NonCompatibleApps!compareDate!.txt" (
-        echo Error: File does not exist.
-        echo "!previousOutputsPath!\NonCompatibleApps!compareDate!.txt"
-		echo.
-        goto :CompareFile
-    )
-    
-    if not exist "!previousOutputsPath!\NonCompatibleComputers!compareDate!.txt" (
-        echo Error: File does not exist.
-        echo "!previousOutputsPath!\NonCompatibleComputers!compareDate!.txt"
-		echo.
-        goto :CompareFile
-    )
-    
-    REM Creates file to store the differences
-    set "DifferencesFile=%axoniusReportsPath%\Differences.txt"
-    echo > "!DifferencesFile!" REM Initializes the differences file
 
+    if not exist "%previousAxoniusReportOutputs%\incompatibleSoftware!compareDate!.txt" (
+		echo Error: Previous incompatible software file does not exist.
+        echo "%previousAxoniusReportOutputs%\incompatibleSoftware!compareDate!.txt"
+        goto :CompareFile
+    )
+    
+    if not exist "%previousAxoniusReportOutputs%\incompatibleHardware!compareDate!.txt" (
+		echo Error: Previous incompatible hardware file does not exist.
+        echo "%previousAxoniusReportOutputs%\incompatibleHardware!compareDate!.txt"
+        goto :CompareFile
+    )
+    
+    set "differencesFile=%axoniusReportsParentDirectory%\differences.txt"
+	> %incompatibleSoftwareFile% echo.
+	
     echo ... Comparing reports
-    
-    REM Compare files and append differences
-    fc "!SortedUniqueNonCompatibleAppsFile!" "!previousOutputsPath!\NonCompatibleApps!compareDate!.txt" >> "!DifferencesFile!" 2>&1
-    fc "!SortedUniqueNonCompatibleComputersFile!" "!previousOutputsPath!\NonCompatibleComputers!compareDate!.txt" >> "!DifferencesFile!" 2>&1
-    
-    REM Open the differences file in Notepad
-    start notepad "!DifferencesFile!"
-
+	
+    fc "%incompatibleSoftwareFile%" "%previousAxoniusReportOutputs%\incompatibleSoftware!compareDate!.txt" >> "!differencesFile!" 2>&1
+    fc "%incompatibleHardwareFile%" "%previousAxoniusReportOutputs%\incompatibleHardware!compareDate!.txt" >> "!differencesFile!" 2>&1
+	
+    start notepad "!differencesFile!"
+	
+	goto :ExitOnSuccess
 ) else if /i "!isCompareFile!"=="n" (
-    goto :EndSuccess
+    goto :ExitOnSuccess
 ) else (
     echo Error: Invalid input.
-	echo.
     goto :CompareFile
 )
 
 
-REM End with success
-:EndSuccess
-echo.
 
+REM This section handles the exit points for the script.
+REM If the script successfully completes, it opens the incompatible hardware and software files in Notepad and then ends the local environment.
+REM If an error occurs, it simply ends the local environment.
 
-REM Opens the output files
-start notepad %SortedUniqueNonCompatibleComputersFile%
-start notepad %SortedUniqueNonCompatibleAppsFile%
-
+:ExitOnSuccess
+start notepad %incompatibleHardwareFile%
+start notepad %incompatibleSoftwareFile%
 endlocal
 
-
-REM End with error
-:EndError
-echo.
+:ExitOnError
 endlocal
